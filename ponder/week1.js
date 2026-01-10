bbca4886d157db45182d7741104edac6906e8272
LandenSpringer <landenspringer96@gmail.com>
2026-01-10 12:15:54 -0700

    added js

---
+++ ponder/week1.js
@@ -0,0 +1,24 @@
+const PI = 3.14;
+let radius = 3;
+
+console.log("PI:", PI);
+console.log("Radius:", radius);
+console.log("Area:", PI * radius * radius);
+
+const one = 1;
+const two = '2';
+
+console.log("one + two =", one + two); 
+console.log("one + Number(two) =", one + Number(two)); 
+
+let course = "CSE131"; 
+
+if (true) {
+    let student = "John";
+    console.log("Course inside block:", course);
+    console.log("Student inside block:", student);
+}
+
+console.log("Course outside block:", course);
+
+console.log(student); 
