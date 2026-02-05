28d724db658abf394afcbd9a7824df89ae01c7a6
LandenSpringer <landenspringer96@gmail.com>
2026-02-05 09:54:33 -0700

    fixed bugs

--- week5/ponder/movie.js
+++ week5/ponder/movie.js
@@ -1,6 +1,6 @@
 function addStars(span) {
     const stars = span.innerText.length;
-    span.setAttribute('aria-label', '${stars} out of 5  stars' );
+    span.setAttribute('aria-label', `${stars} out of 5 stars` );
 }
 
 const spans = document.querySelectorAll('article.movie p span');
