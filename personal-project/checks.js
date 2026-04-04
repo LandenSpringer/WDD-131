359f7e7458c7d36080ebfbe155a1c44c4e67c41e
LandenSpringer <landenspringer96@gmail.com>
2026-04-04 01:15:06 -0600

    formatting

--- personal-project/checks.js
+++ personal-project/checks.js
@@ -1,19 +1,19 @@
 export function hasLength(pw) {
-  return pw.length >= 8;
+    return pw.length >= 8;
 }
 
 export function hasUpper(pw) {
-  return /[A-Z]/.test(pw);
+    return /[A-Z]/.test(pw);
 }
 
 export function hasLower(pw) {
-  return /[a-z]/.test(pw);
+    return /[a-z]/.test(pw);
 }
 
 export function hasNumber(pw) {
-  return /[0-9]/.test(pw);
+    return /[0-9]/.test(pw);
 }
 
 export function hasSymbol(pw) {
-  return /[^A-Za-z0-9]/.test(pw);
+    return /[^A-Za-z0-9]/.test(pw);
 }
